d8b55f493eb09ce7b32a377d32ccba130731d30f
Pavithra K <pksjce@gmail.com>
2015-07-21 16:41:49 +0530

    Remove container at destroy

--- addon/components/ember-dragula-container.js
+++ addon/components/ember-dragula-container.js
@@ -6,7 +6,7 @@ export default Ember.Component.extend({
 			this.parent.drake.containers.push(this.element)
 		}.bind(this))
 	},
-	actions:{
-
+	willDestroyElement:function(){
+		this.parent.drake.containers.remove(this.element);
 	}
 });
