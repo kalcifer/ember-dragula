ee880f5ef91089afdb41e43c4e30c28f5f0b7d3f
Eric Douglas <eric.douglas.mail@gmail.com>
2015-08-26 17:11:51 -0300

    Fix and update dragula name/version

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
   name: 'ember-dragula',
   included: function(app) {
     this._super.included(app);
-    app.import(app.bowerDirectory + '/dragula.js/dist/dragula.js');
-	app.import(app.bowerDirectory + '/dragula.js/dist/dragula.css');
+    app.import(app.bowerDirectory + '/dragula/dist/dragula.js');
+    app.import(app.bowerDirectory + '/dragula/dist/dragula.css');
   },
 };
