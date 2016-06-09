af8b6101433c71d7cc5b7e281be2abbe14f219b4
Rômulo Machado <romulo.machado5@gmail.com>
2016-06-09 17:39:43 -0300

    Only import the bower component outside of fastboot

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ module.exports = {
   name: 'ember-dragula',
   included: function(app) {
     this._super.included(app);
-    app.import(app.bowerDirectory + '/dragula/dist/dragula.js');
     app.import(app.bowerDirectory + '/dragula/dist/dragula.css');
+
+    if (!process.env.EMBER_CLI_FASTBOOT) {
+      // If this flag is present, the addon is being built in FastBoot.
+      // The bower component causes FastBoot to crash, so only import in
+      // the browser build
+      app.import(app.bowerDirectory + '/dragula/dist/dragula.js');
+    }
   },
 };
